7ed0f8b3f3952eb462ea369fcf32784ca6a3cc6b
carolmuchoki95 <carolmuchoki95@gmail.com>
2018-06-09 12:58:06 +0300

    Button functionality

--- app/src/main/java/com/muchoki/carol/alc/MainActivity.java
+++ app/src/main/java/com/muchoki/carol/alc/MainActivity.java
@@ -2,12 +2,22 @@ package com.muchoki.carol.alc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+     Button mLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLogin=(Button) findViewById(R.id.btnLogin);
+    }
+
+    public void doIt(View view) {
+        Toast.makeText(getApplicationContext(), "Hello welcome to ALC", Toast.LENGTH_LONG).show();
     }
 }
